ba83343ca94849344e2066278e50dd4b4e04c64d
Yuriy Shotin <yshotin@griddynamics.com>
2020-01-15 04:33:17 -0500

    STEP 3.05 Adding State to a React Counter Component

--- todo-app/src/components/counter/Counter.jsx
+++ todo-app/src/components/counter/Counter.jsx
@@ -1,27 +1,49 @@
 import React, { Component } from 'react';
 import './Counter.css';
 
-function Counter() {
-    return (
-        <div className="counter">
-            <h1>Counter</h1>
-            <div>
-            <button onClick={increment}>+1</button>
-            <button onClick={decrement}>-1</button>
+class Counter extends Component {
+
+    // Define the initial state in a constructor
+    // state => counter = 0
+    constructor() {
+        super(); // Error number one - not calling super()
+        this.state = {
+            counter : 0
+        }
+        this.increment = this.increment.bind(this);
+        this.decrement = this.decrement.bind(this);
+    }
+
+    render() {
+        return (
+            <div className="counter">
+                <h1>Counter</h1>
+                <div>
+                <button onClick={this.increment}>+1</button>
+                <button onClick={this.decrement}>-1</button>
+                </div>
+                <div className="count">
+                <span >{this.state.counter}</span>
+                </div>
             </div>
-            <div className="count">
-            <span >0</span>
-            </div>
-        </div>
-    );
+        );
+    };
+
+    increment() {
+        console.log('increment');
+        this.setState({
+            counter: this.state.counter + 1
+        });
+    };
+    
+    decrement() {
+        console.log('decrement');
+        this.setState({
+            counter: this.state.counter - 1
+        });
+    };
 }
 
-function increment() {
-    console.log('increment');
-}
 
-function decrement() {
-    console.log('decrement');
-}
 
 export default Counter;
