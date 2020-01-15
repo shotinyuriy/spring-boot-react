3a45f152aefdf72f7ed5a6ac9c6a190a14e6b842
Yuriy Shotin <yshotin@griddynamics.com>
2020-01-15 04:17:47 -0500

    STEP 3.03 Adding a Button and a Counter to the React Counter component

--- todo-app/src/components/counter/Counter.jsx
+++ todo-app/src/components/counter/Counter.jsx
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
+import './Counter.css';
 
 function Counter() {
     return (
         <div className="counter">
             <h1>Counter</h1>
+            <div>
+            <button>+1</button>
+            <button>-1</button>
+            </div>
+            <div className="count">
+            <span >0</span>
+            </div>
         </div>
     );
 }
