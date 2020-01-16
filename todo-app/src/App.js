5804e5035fabe7884736eadf00ae7336f2920ae8
Yuriy Shotin <yshotin@griddynamics.com>
2020-01-16 08:20:46 -0500

    STEP 3.12 Moving State Up - Refactoring to Counter and Counter Button components

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -12,9 +12,6 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <Counter by={1}></Counter>
-                <Counter by={5}></Counter>
-                <Counter by={10}></Counter>
                 <Counter></Counter>
             </div>
         );
--- todo-app/src/components/counter/Counter.jsx
+++ todo-app/src/components/counter/Counter.jsx
@@ -3,6 +3,20 @@ import PropTypes from 'prop-types';
 import './Counter.css';
 
 class Counter extends Component {
+    render() {
+        return (
+            <div className="counter">
+                <h1>Counter</h1>
+                <CounterButton by={1}></CounterButton>
+                <CounterButton by={5}></CounterButton>
+                <CounterButton by={10}></CounterButton>
+                <CounterButton></CounterButton>
+            </div>
+        );
+    }
+}
+
+class CounterButton extends Component {
 
     // Define the initial state in a constructor
     // state => counter = 0
@@ -18,16 +32,15 @@ class Counter extends Component {
     render() {
         const counterStyle = {fontFamily: 'Arial', color: '#444444'};
         return (
-            <div className="counter">
-                <h1>Counter</h1>
+                <>
                 <div>
-        <button onClick={this.increment}>+{this.props.by}</button>
+                <button onClick={this.increment}>+{this.props.by}</button>
                 <button onClick={this.decrement}>-{this.props.by}</button>
                 </div>
                 <div className="count" style={counterStyle}>
                 <span >{this.state.counter}</span>
                 </div>
-            </div>
+                </>
         );
     };
 
@@ -46,11 +59,11 @@ class Counter extends Component {
     };
 }
 
-Counter.defaultProps = {
+CounterButton.defaultProps = {
     by: 1
 }
 
-Counter.propTypes = {
+CounterButton.propTypes = {
     by: PropTypes.number
 }
 
