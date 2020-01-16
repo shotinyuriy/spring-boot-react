fe057cc0952d63c62044274915b8621a3117bc36
Yuriy Shotin <yshotin@griddynamics.com>
2020-01-16 09:13:50 -0500

    STEP 3.13 Moving State Up - Adding state to Counter Component

--- todo-app/src/components/counter/Counter.jsx
+++ todo-app/src/components/counter/Counter.jsx
@@ -3,16 +3,35 @@ import PropTypes from 'prop-types';
 import './Counter.css';
 
 class Counter extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            counter : 0
+        };
+        this.increment = this.increment.bind(this);
+    }
+
     render() {
         return (
             <div className="counter">
                 <h1>Counter</h1>
-                <CounterButton by={1}></CounterButton>
-                <CounterButton by={5}></CounterButton>
-                <CounterButton by={10}></CounterButton>
-                <CounterButton></CounterButton>
+                <CounterButton title="Peter" by={1} incrementMethod={this.increment}></CounterButton>
+                <CounterButton title="John" by={5} incrementMethod={this.increment}></CounterButton>
+                <CounterButton title="Barnie" by={10} incrementMethod={this.increment}></CounterButton>
+                <CounterButton title="Melissa" incrementMethod={this.increment}></CounterButton>
+                <div>
+                    Total: {this.state.counter}
+                </div>
             </div>
         );
+    }
+
+    increment(by) {
+        console.log(`increment in parent by ${by}`);
+        this.setState({
+            counter: (this.state.counter + by)
+        });
     }
 }
 
@@ -26,21 +45,20 @@ class CounterButton extends Component {
             counter : 0
         }
         this.increment = this.increment.bind(this);
-        this.decrement = this.decrement.bind(this);
     }
 
     render() {
         const counterStyle = {fontFamily: 'Arial', color: '#444444'};
         return (
-                <>
-                <div>
+                <div className="counterLine">
+                    <span className="title">{this.props.title}:</span>
+                <span>
                 <button onClick={this.increment}>+{this.props.by}</button>
-                <button onClick={this.decrement}>-{this.props.by}</button>
-                </div>
-                <div className="count" style={counterStyle}>
+                </span>
+                <span className="count" style={counterStyle}>
                 <span >{this.state.counter}</span>
+                </span>
                 </div>
-                </>
         );
     };
 
@@ -49,13 +67,7 @@ class CounterButton extends Component {
         this.setState({
             counter: this.state.counter + this.props.by
         });
-    };
-    
-    decrement() {
-        // this.state.counter-- => Bad Practice
-        this.setState({
-            counter: this.state.counter - this.props.by
-        });
+        this.props.incrementMethod(this.props.by);
     };
 }
 
